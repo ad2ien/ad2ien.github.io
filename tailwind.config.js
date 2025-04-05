a3d713fee483e733681c8fa50bb83ed78ebd9dd3
Adrien Touzouli <contact@ad2ien.dev>
2025-04-05 22:03:41 +0200

    ⬆️ upgrade dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,23 @@
 module.exports = {
-   purge: [
-      './templates/**/*.html',
-      './static/js/**/*.js',
-   ],
-  darkMode: 'class', // or 'media' or 'class'
+  content: ["./templates/**/*.html", "./static/js/**/*.js"],
+  darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {
       width: {
-        a4: '210mm',
-        almosthalf: '49.2%'
-     },
-     height: {
-       a4: '297mm',
-     },
-     screens: {
-      print: { raw: 'print' },
-      screen: { raw: 'screen' },
-    },
+        a4: "210mm",
+        almosthalf: "49.2%",
+      },
+      height: {
+        a4: "297mm",
+      },
+      screens: {
+        print: { raw: "print" },
+        screen: { raw: "screen" },
+      },
     },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
